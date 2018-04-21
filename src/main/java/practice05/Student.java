7f2405f3c4ec3bbdf7e81b95e6cb48512dfaf01a
Angel-kou <1844646307@qq.com>
2018-04-21 15:43:53 +0800

    same as practice04 .written by kmj 2018/4/21 15:43

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,26 @@
 package practice05;
 
-public class Student {
+public class Student extends Person{
+    private String name;
+    private int age;
+    private int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String intro = super.introduce() + " I am a Student. I am at Class 2.";
+        return intro;
+    }
 }
