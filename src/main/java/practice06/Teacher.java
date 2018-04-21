1ebbbb8581d5d099d3eeacaba8b53862128adea1
Angel-kou <1844646307@qq.com>
2018-04-21 15:45:43 +0800

    add attribute kclass for class Teacher, change introduce methodby kmj 2018/4/21 15:45

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,35 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private String name;
+    private int age;
+    private int klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String basic = super.introduce();
+        if(this.klass == 0){
+            basic += " I am a Teacher. I teach No Class.";
+        }else{
+            basic += " I am a Teacher. I teach Class 2.";
+        }
+        return basic;
+    }
 }
