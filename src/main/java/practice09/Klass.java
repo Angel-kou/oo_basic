ca4d1136ffcf32571d8821f41227cbec7ca01233
Angel-kou <1844646307@qq.com>
2018-04-21 15:51:07 +0800

    add appendNumber method and when some student add to a class,we need to show message . written by kmj 2018/4/21 15:51

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,52 @@
 package practice09;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private int number;
+    private Student leader;
+    private List<Student> members = new ArrayList<>();
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public void assignLeader(Student leader){
+        if(members.contains(leader)){
+            this.leader = leader;
+        }else{
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return "Class "+this.number;
+    }
+
+
+    public List<Student> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Student> members) {
+        this.members = members;
+    }
+
+    public void appendMember(Student student){
+        this.members.add(student);
+    }
 }
+
