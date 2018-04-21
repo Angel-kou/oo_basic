20d6b0414f0dfd984831df5a83aa510b48184197
Angel-kou <1844646307@qq.com>
2018-04-21 15:54:00 +0800

    change attribute of Teacher into classes,which is a hashSet and judge the number of klass , modify the message . written by kmj 2018/4/21 15:53

--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,65 @@
 package practice10;
 
-public class Teacher {
+import java.util.*;
+
+public class Teacher extends Person{
+    private HashSet<Klass> classes = new HashSet<>();
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, HashSet<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+    }
+
+    public Set<Klass> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(HashSet<Klass> classes) {
+        this.classes = classes;
+    }
+
+    @Override
+    public String introduce() {
+        System.out.println("--111----");
+        String basic = super.introduce();
+        if(this.classes.size() == 0){
+            basic += " I am a Teacher. I teach No Class.";
+        }else{
+            basic += " I am a Teacher. I teach Class ";
+            List<Integer> list = new ArrayList<>();
+            this.classes.stream().forEach(x->list.add(x.getNumber()));
+            list.sort(Comparator.naturalOrder());
+            for(int i= 0; i <= list.size()-1 ;i++){
+                if(i != list.size()-1){
+                    basic += list.get(i)+", ";
+                }else{
+                    basic += list.get(i)+".";
+                }
+            }
+
+        }
+        return basic;
+    }
+
+    public boolean isTeaching(Student student){
+        if(this.classes.contains(student.getKlass())){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public String introduceWith(Student student){
+        String basic = super.introduce();
+        if(this.classes.contains(student.getKlass())){
+            basic += " I am a Teacher. I teach "+student.getName()+".";
+        }else{
+            basic += " I am a Teacher. I don't teach "+student.getName()+".";
+        }
+        return basic;
+    }
 }
