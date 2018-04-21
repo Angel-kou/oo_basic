1c289ab2bf05e8801b83ddfa452a9ddd7507f9d4
Angel-kou <1844646307@qq.com>
2018-04-21 15:49:15 +0800

    add assignLeader method for Klass by kmj 2018/4/21 15:49

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,32 @@
 package practice08;
 
+import java.util.List;
+
 public class Klass {
+    private int Number;
+    private Student leader;
+
+    public Klass(int number) {
+        Number = number;
+    }
+
+    public void assignLeader(Student leader){
+        this.leader = leader;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public int getNumber() {
+        return Number;
+    }
+
+    public void setNumber(int number) {
+        Number = number;
+    }
+
+    public String getDisplayName() {
+        return "Class "+this.Number;
+    }
 }
--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,46 @@
 package practice08;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int id;
+    private String name;
+    private int age;
+    private Klass klass;
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String basic = super.introduce();
+        if(this.klass==null || this.klass.getNumber() == 0){
+            basic += " I am a Teacher. I teach No Class.";
+        }else{
+            basic += " I am a Teacher. I teach Class 2.";
+        }
+        return basic;
+    }
+
+    public String introduceWith(Student student){
+        String basic = super.introduce();
+        if(student.getKlass().getNumber()==this.klass.getNumber()){
+            basic += " I am a Teacher. I teach "+student.getName()+".";
+        }else{
+            basic += " I am a Teacher. I don't teach "+student.getName()+".";
+        }
+        return basic;
+    }
 }
